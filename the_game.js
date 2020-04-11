5fe864cd490dba1655f2e263581e719c916a38d9
Drew Johnson (samsung tablet) <werd2.718@gmail.com>
2020-04-11 22:28:59 +0200

    Update the_game.js

--- the_game.js
+++ the_game.js
@@ -1,10 +1,23 @@
+var score = 0
+
 function input_button_pushed(){
     var ans = document.getElementById("the_input").value;
 
     var para = document.createElement("p");
 
     if (ans in names_to_order){
-
+        var order = names_to_order[ans]
+        if (!order_to_done[order]){
+            order_to_done[order] = true
+            score = score + 1
+            var node = document.createTextNode(order + " : " + ans)
+            para.appendChild(node)
+            var the_div = document.getElementById("results_div")
+            the_div.appendChild(para)
+        }
+        else{
+            alert("already used that!")
+        }
     }
     else{
         var node = document.createTextNode(ans);
@@ -14,6 +27,11 @@ function input_button_pushed(){
     }
 }
 
+var order_to_done ={
+    "number" :false,
+    "color" : false,
+    "letter": false
+}
 var names_to_order = {
     "one" : "number",
     "two" : "number",
